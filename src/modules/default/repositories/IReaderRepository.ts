b2fd6001f779dd24fbaa8146ec2c70dc69bd1983
Pedro Dalpa <pedro.dalpa@gmail.com>
2025-03-20 15:48:34 -0300

    chore: remove default module and related components including controller, use case and repository

--- src/modules/default/repositories/IReaderRepository.ts
+++
@@ -1,8 +0,0 @@
-import { ReaderModel } from '../model/ReaderModel'
-
-interface IReaderRepository {
-  findByEmail(email: string): Promise<ReaderModel | null>
-  save(props: ReaderModel): Promise<void>
-}
-
-export { IReaderRepository }
